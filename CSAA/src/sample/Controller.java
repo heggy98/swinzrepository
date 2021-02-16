0c64a59622ff7002343dc17b54da6092eda8a9c2
heggy98 <dominikheger98@gmail.com>
2021-02-16 17:41:11 +0100

    Tlačítko teď zatím vypíše do konzole potvrzení :)

--- CSAA/src/sample/Controller.java
+++ CSAA/src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
+
 public class Controller {
+    @FXML
+    private void buttonUlozitAction(ActionEvent event) {
+        System.out.println("tak to máš uložený no :)");
+    }
 }
